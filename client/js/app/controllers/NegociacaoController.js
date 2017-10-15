9c39eb98f13658190ef854d1d266295a8db40957
pedro-inacio11 <pedro.inacio11@gmail.com>
2017-10-15 20:36:02 -0200

    Ajustes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -17,29 +17,14 @@ class NegociacaoController {
 
     event.preventDefault();
 
-    // O formato do Date começa do 0 ao 11, ou seja Janeiro é igual ao mês 0 ..
-    let data = new Date(
-      //Os ... me permite passar um array como parametro para um construtor de uma classe ou uma função..
-      //Mas quando uso o ... quero dizer que o primeiro item do array vai ser um parametro
-      // passado para esse construtor e assim por diante.
-      ...this._inputData.value
-      //tranformando nossa string num array e nesse array realizar o ajuste através da função map
-             .split('-')
-             // utilizei a arrowFunction, quando essa arrowFunction só tem uma instrução eu não preciso passar
-             // um bloco, podendo omitir o return sempre trazendo o valor --> item - indice % 2
-             .map((item, indice) => item - indice % 2)
-           );
+    let negociacao = new Negociacao(
+        DateHelper.textoParaData(this._inputData.value),
+        this._inputData.value,
+        this._inputValor
+      );
 
-           let negociacao = new Negociacao(
-             data,
-             this._inputData.value,
-             this._inputValor
-           );
+      console.log(negociacao);
+      console.log(DateHelper.dataParaTexto(negociacao.data));
 
-           let diaMesAno = negociacao.data.getDate()
-              + '/' + (negociacao.data.getMonth() + 1)
-              + '/' + negociacao.data.getFullYear();
-
-           console.log(diaMesAno);
     }
 }
