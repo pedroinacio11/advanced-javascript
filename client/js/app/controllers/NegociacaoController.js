1f75333bf083498ca0ea4243ba14c47bb6585f07
pedro-inacio11 <pedro.inacio11@gmail.com>
2017-10-15 21:17:24 -0200

    Blidando a Lista de Negiações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,21 +10,33 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._listaNegociacoes = new ListaNegociacoes();
 
   }
 
   adiciona(event){
-
     event.preventDefault();
+    this._listaNegociacoes.adiciona(this._criarNegociacao());
+    this._limpaFormulario();
+    }
 
-    let negociacao = new Negociacao(
-        DateHelper.textoParaData(this._inputData.value),
-        this._inputData.value,
-        this._inputValor
-      );
+  _criarNegociacao(){
 
-      console.log(negociacao);
-      console.log(DateHelper.dataParaTexto(negociacao.data));
+      return new Negociacao(
+    DateHelper.textoParaData(this._inputData.value),
+    this._inputQuantidade.value,
+    this._inputValor.value);
 
     }
+
+    _limpaFormulario() {
+
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+
+    this._inputQuantidade.focus();
+
+    }
+
 }
