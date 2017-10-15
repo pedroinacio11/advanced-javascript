6e3a351719f95b9c021d07b249f908e970a428e1
pedro-inacio11 <pedro.inacio11@gmail.com>
2017-10-15 20:48:43 -0200

    Usando Expressões Regulares

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -12,6 +12,11 @@ class DateHelper {
     }
 
   static textoParaData(texto){
+
+    //Expressão regular - Numero de 4 dig ... + Numero de 2 digitos ...
+    if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+      throw Error('Deve estar no formato aaaa-mm-dd');
+
     //tranformando nossa string num array e nesse array realizar o ajuste através da função map
            return new Date(...texto.split('-')
            // utilizei a arrowFunction, quando essa arrowFunction só tem uma instrução eu não preciso passar
