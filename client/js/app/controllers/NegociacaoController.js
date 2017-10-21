f25118ad2c38b6d61ff257596911531484aa6a72
pedro-inacio11 <pedro.inacio11@gmail.com>
2017-10-21 09:17:49 -0200

    Requisções AJAX

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -32,6 +32,53 @@ class NegociacaoController {
     this._limpaFormulario();
     }
 
+    importaNegociacoes(){
+
+      //crio uma instancia de XMLHTTPRequest
+      let xhr = new XMLHttpRequest();
+
+      //Indico qual endereço quero acessar (se não for local precisa do endereço completo)
+      xhr.open('GET', 'negociacoes/semana');
+
+      /* Configurações dessa minha requisição Ajax */
+      /* Eu preciso saber qual estado eu estou quando onreadystatechange for chamado */
+      xhr.onreadystatechange = () => {
+
+        /*
+        ESTADOS DE UMA REQUISIÇÃO AJAX
+        0: requisição ainda não iniciada
+        1: conexão com servidor estabelecida
+        2: requisição recebida
+        3: processando requisição
+        4: requisição concluida e a resposta esta pronta
+        */
+
+        if(xhr.readyState == 4 ){
+
+          if(xhr.status == 200){
+            console.log('Obtendo as negociações do servidor');
+
+            JSON.parse(xhr.responseText) // fiz o parse que vai tranformar num array de objetos ..
+            /*Eu precorro cada array de objetos para criar um novo array, 
+            onde cada objeto desse eu instancio uma nova negociação*/
+                .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                //agora dessa lista que eu criei com o map -- iitero sobre ela e adiciona cada item na minha lista de 
+                // negociações ... 
+                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações importadas com sucesso';
+
+          }else {
+            console.log(xhr.responseText);
+            this._mensagem.texto = 'Não foi possivel obter as negoaciações da semana.';
+          }
+        }
+      };
+
+      //E Envio
+      xhr.send();
+
+    }
+
     apaga(){
 
       this._listaNegociacoes.esvazia();
